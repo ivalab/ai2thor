e905263329da9408fc2d8d5b5a58be0c441a5275
Ruinian Xu <rxu72@gatech.edu>
2021-08-10 17:49:19 -0400

    add some test scripts for testing local built aithor

---
+++ tests/controller_test.py
@@ -0,0 +1,19 @@
+import __init__paths
+
+import ai2thor.controller
+import ai2thor.fifo_server
+import ai2thor.wsgi_server
+
+controller = ai2thor.controller.Controller(local_executable_path = '/home/ruinian/IVALab/Project/TaskGrounding/ai2thor/unity/builds/thor-Linux64-local/thor-Linux64-local',
+                                           scene="FloorPlan1",
+                                           # image modalities
+                                           renderDepthImage=True,
+                                           renderInstanceSegmentation=True,
+                                           # camera properties
+                                           width=640,
+                                           height=480,)
+
+while 1:
+    print(controller.last_event.metadata['agent']['projectionMatrix'])
+    continue
+    
